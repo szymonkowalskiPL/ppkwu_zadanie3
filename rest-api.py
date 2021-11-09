4130740c7a8d5040ee8d9ea15ea04c3fa079ad21
szkowalski <222477@edu.p.lodz.pl>
2021-11-09 12:59:43 +0100

    response types added

--- rest-api.py
+++ rest-api.py
@@ -10,8 +10,18 @@ def checkString():
     responsType = request.args.get('responseType')
     link = 'http://127.0.0.1:5000/checkstring?string='+string
     print(link)
-    requests.post(link).content
-    
-    return "OK 200"
+    result = requests.post(link)
+    data = result.json()
+
+    if responsType==1:
+         print("txt reponse")
+    elif responsType==2:
+        print("json reponse")
+    elif responsType==3:
+        print("xml response")
+    elif responsType==4:
+        print("csv response")
+
+    return "ok"
 
 app.run(host="localhost", port=8000, debug=False)
