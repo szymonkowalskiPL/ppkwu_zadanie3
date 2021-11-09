443eefd98bc4b3ec6d3d59fbd91e559449c22810
szkowalski <222477@edu.p.lodz.pl>
2021-11-09 12:44:50 +0100

    first try connecting to api2 failed

--- rest-api.py
+++ rest-api.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 
 @app.route('/checkstring',  methods=['GET', 'POST'])
 def checkString():
-    
     string = request.args.get('string')
-    print(string)
-    responseType = request.args.get('responseType')
-    print(responseType)
-
-    
+    responsType = request.args.get('responseType')
+    requests.get('http://127.0.0.1:5000/checkstring?string='+string).content
     
     return "OK 200"
 
-app.run(host="localhost", port=8000, debug=True)
+app.run(host="localhost", port=8000, debug=False)
